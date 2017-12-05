dc82fdc03063d36602f30e58074da1766b38dd6b
NiuYulong <15011116026@163.com>
2017-12-05 21:50:51 +0800

    初始化项目：虚拟环境，安装python3.6对应的django库(2.0)

---
+++ py3env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/niuyulong/Desktop/MyHarvest/MyHarvest-Server/py3env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
